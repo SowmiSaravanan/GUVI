c89e64db011893d3e1e157a69d633c60bb377f08
Sowmiya <41140103+SowmiSaravanan@users.noreply.github.com>
2018-07-12 16:05:40 +0530

    Update nk.py

--- nk.py
+++ nk.py
@@ -2,9 +2,9 @@ n=int(input())
 k=int(input())
 lis=[]
 sum=0
-for i in range(lis):
+for i in range(n):
   num=int(input())
   lis.append(num)
-for i in range(len(lis)):
-  sum=sum+i
+for i in range(k):
+  sum=sum+lis[i]
 print (sum)

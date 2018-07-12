18b27c3705c07e0589a1b3446ab08b643c0bd20c
Sowmiya <41140103+SowmiSaravanan@users.noreply.github.com>
2018-07-12 15:03:16 +0530

    Update large3.py

--- large3.py
+++ large3.py
@@ -1,6 +1,8 @@
-num1=int(input())
-num2=int(input())
-num3=int(input())
+x=input()
+x=x.split()
+num1=int(x[0])
+num2=int(x[1])
+num3=int(x[2])
 if num1>=num2 and num1>=num3:
   print(num1)
 elif num2>=num1 and num2>=num3:

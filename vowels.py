d6dd376ffdba1b4d00a0f62efeb464248f710be1
Sowmiya <41140103+SowmiSaravanan@users.noreply.github.com>
2018-07-12 14:34:24 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -3,5 +3,5 @@ if al=='a' or al=='e' or al=='o' or al=='i' or al=='u':
   print("Vowel")
 elif al=='#' or al=='&' or al=='$'or al=='@':
   print("Invalid")
-else:
+elif ord(al)>97 and ord(al)<122:
   print("Consonant")
